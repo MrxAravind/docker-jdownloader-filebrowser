c44ad47b14b18122a3688fb0302e7a7fc21d0fd9
devolart <125961492+devolart@users.noreply.github.com>
2024-04-04 16:10:23 +0700

    Revert: v.1.2

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
-if [[ -z "${FB_USERNAME}" ]] || [[ -z "${FB_PASSWORD}" ]]; then
-    ./filebrowser --username "$FB_USERNAME" --password "$(./filebrowser hash $FB_PASSWORD)" &
-    ./docker-entrypoint.sh
-else
-    ./filebrowser -a "0.0.0.0" &
-    ./docker-entrypoint.sh
-fi
+./filebrowser -a "0.0.0.0" &
+./docker-entrypoint.sh
