837381675d07d9d8e8b6cde93b4f20092438fb6c
devolart <125961492+devolart@users.noreply.github.com>
2024-04-04 08:55:32 +0000

    v1.2

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
-./filebrowser -a "0.0.0.0" &
-./docker-entrypoint.sh
+if [[ -z "${FB_USERNAME}" ]] || [[ -z "${FB_PASSWORD}" ]]; then
+    ./filebrowser --username "$FB_USERNAME" --password "$(./filebrowser hash $FB_PASSWORD)" &
+    ./docker-entrypoint.sh
+else
+    ./filebrowser -a "0.0.0.0" &
+    ./docker-entrypoint.sh
+fi
